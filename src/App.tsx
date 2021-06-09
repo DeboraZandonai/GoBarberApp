59cca5edbf5b9554e178fc2938edc8418e0e2a56
Debora Zandonai <debohzandonaii@gmail.com>
2021-06-08 23:47:34 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StatusBar } from "react-native";
+
+const App: React.FC = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38"></StatusBar>
+    <View style={{ backgroundColor: "#312e38", flex: 1 }} />;
+  </>
+);
+export default App;
